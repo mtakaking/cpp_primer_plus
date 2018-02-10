8cfcc6d9fbda182aa99d0f0f9315aa2853cbca3d
Takaki Matsumoto <mtakaking@users.noreply.github.com>
2018-02-09 21:51:15 -0500

    chapter 2

---
+++ cpp_primer_plus/chapter2/1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    string myName;
+    string myAddress;
+    cout << "What is your name?" << endl;
+    cin >> myName;
+    cout << "What is your address?" << endl;
+    cin >> myAddress;
+    cout << "Your Name is: " << myName << endl;
+    cout << "Your Address is: " << myAddress << endl;
+    return 0;
+}
